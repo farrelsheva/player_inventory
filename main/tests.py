5e36174389bd70e8b543c406509204270239ec99
farrelsheva <farrelshevaa@gmail.com>
2023-09-11 20:45:04 +0700

    finished table

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from main.models import Item
 
 # Create your tests here.
+class mainTest(TestCase):
+    
+    def test_main_page_context(self):
+        client = Client()
+        response = client.get('/main/')
+        
+        self.assertEqual(response.context['name'], ['Sword', 'Shield', 'Potion'])
+        self.assertEqual(response.context['description'], ['A sword made of steel', 'A shield made of steel', 'A potion of healing'])
+        
+        
--- main/views.py
+++ main/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_main(request):
+    names = ['Sword', 'Shield', 'Potion']
+    descriptions = [
+                    'A sword made of steel', 
+                    'A shield made of steel', 
+                    'A potion of healing'
+                    ]
+    amount = [1, 1, 4]
+
+    zipped = zip(names, descriptions, amount)
     context = {
-        'name': 'Sword',
-        'description': 'A sword made of steel'
+        'zipped': zipped
         
     }
     return render(request, 'main.html', context)
