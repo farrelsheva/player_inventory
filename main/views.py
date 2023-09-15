e9838fbc724320a048ac4f4a456634e61b0f3d28
farrelsheva <farrelshevaa@gmail.com>
2023-09-15 22:29:21 +0700

    deprecated old table, working on tugas 3

--- main/views.py
+++ main/views.py
@@ -2,17 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_main(request):
-    names = ['Sword', 'Shield', 'Potion']
-    descriptions = [
-                    'A sword made of steel', 
-                    'A shield made of steel', 
-                    'A potion of healing'
-                    ]
-    amount = [1, 1, 4]
-
-    zipped = zip(names, descriptions, amount)
+    
     context = {
-        'zipped': zipped
+        'name' : "Farrel Sheva Alkautsar",
+        'class' : "PBP A",
+        'npm' : "2206030344"
         
     }
     return render(request, 'main.html', context)
