66e73523a895d02c4c3543ce9583bb548f83d1c2
farrelsheva <farrelshevaa@gmail.com>
2023-09-17 14:52:37 +0700

    added images of postman

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from main.views import show_main, create_item
+from main.views import show_main, create_item, show_json, show_xml, show_xml_by_id, show_json_by_id
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-    path('create-item', create_item, name='create_item')
+    path('create-item', create_item, name='create_item'),
+
+    path('json/', show_json, name='show_json'),
+    path('xml/', show_xml, name='show_xml'),
+    path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id')
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from main.forms import ItemForm
 from django.urls import reverse
 from main.models import Item
+from django.core import serializers
 # Create your views here.
 def show_main(request):
     items = Item.objects.all()
@@ -24,5 +25,26 @@ def create_item(request):
     
     context = {'form': form}
     return render(request, 'create_item.html', context)
-    
-    
+
+def show_html(request):
+    data = Item.objects.all()
+    context = {
+        'items': data
+    }
+    return render(request, 'show_html.html',context)
+
+def show_json(request):
+    data = Item.objects.all()
+    return(HttpResponse(serializers.serialize("json", data), content_type="application/json"))
+
+def show_xml(request):
+    data = Item.objects.all()
+    return(HttpResponse(serializers.serialize("xml", data), content_type="application/xml"))
+
+def show_xml_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return(HttpResponse(serializers.serialize("xml", data), content_type="application/xml"))     
+
+def show_json_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return(HttpResponse(serializers.serialize("json", data), content_type="application/json"))
