9bb7c51955c8556989a1841f8fc5194a93cebe39
farrelsheva <farrelshevaa@gmail.com>
2023-09-24 18:20:42 +0700

    added basic auth functions, login restriction

--- main/views.py
+++ main/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
 from main.forms import ItemForm
 from django.urls import reverse
 from main.models import Item
 from django.core import serializers
 # Create your views here.
+
+@login_required(login_url='/login')
 def show_main(request):
     items = Item.objects.all()
     context = {
@@ -62,3 +66,22 @@ def register(request):
             return redirect('main:login')
     context = {'form':form}
     return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main:show_main')
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('main:login')
+
+    
